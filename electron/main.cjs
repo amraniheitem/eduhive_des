20637eb7934746934ddfea0fb5f5528bba98304e
Amrani Heitem <amraniheitem@gmail.com>
2026-03-29 13:49:16 +0100

    v1.0.1

--- electron/main.cjs
+++ electron/main.cjs
@@ -1,6 +1,8 @@
 const { app, BrowserWindow } = require('electron')
 const path = require('path')
 
+const isDev = process.env.NODE_ENV === 'development'
+
 function createWindow() {
     const win = new BrowserWindow({
         width: 1200,
@@ -11,11 +13,20 @@ function createWindow() {
         }
     })
 
-    win.loadFile(path.join(__dirname, '../dist/index.html'))
+    if (isDev) {
+        win.loadURL('http://localhost:5173')
+        win.webContents.openDevTools()
+    } else {
+        win.loadFile(path.join(__dirname, '../dist/index.html'))
+    }
 }
 
 app.whenReady().then(createWindow)
 
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
 })
